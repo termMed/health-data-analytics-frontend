78d90d63743f525a00bc6d2e4a4e17e9b9829304
Patricio Gayol <patricio@termmed.com>
2019-03-27 12:15:28 -0300

    Fixed wrong function

--- app/controllers/cohorts.js
+++ app/controllers/cohorts.js
@@ -74,11 +74,11 @@ export default Ember.Controller.extend({
             this.set('model.gender', $('#genderSelect').find(":selected").text());
             if(this.get('model.gender') !== null && this.get('model.gender') !== undefined){
                 //Changes to make it work in spanish translation
-                if(this.get('model.gender') == 'Masculino'){
-                    this.get('model.gender') = 'Male';
+                if(this.set('model.gender') == 'Masculino'){
+                    this.set('model.gender') = 'Male';
                 }
-                if(this.get('model.gender') == 'Femenino'){
-                    this.get('model.gender') = 'Female';
+                if(this.set('model.gender') == 'Femenino'){
+                    this.set('model.gender') = 'Female';
                 }
                 gender = this.get('model.gender').toUpperCase();
             }
