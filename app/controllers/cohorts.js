710136274937473b261249c6ab9af57cb3608bd8
Patricio Gayol <patricio@termmed.com>
2019-04-16 14:34:29 -0300

    Changes in fetch cohort to request the result concepts in snowstorm

--- app/controllers/cohorts.js
+++ app/controllers/cohorts.js
@@ -199,9 +199,11 @@ export default Ember.Controller.extend({
                             
                             this.set('loading', false); 
                             result.content.forEach(function(item){
+                                console.log('Iterating content');
                                 item.encounters.forEach(function(encounter){
+                                    console.log('Iterating encounters')
                                     var conceptId = encounter.conceptId;
-                                    this.get('ajax').request('/find/MAIN/SNOMEDCT-ES/SNOMEDCT-URU/concepts/' + conceptId).then((result) => {
+                                    var translation = this.get('ajax').request('/find/MAIN/SNOMEDCT-ES/SNOMEDCT-URU/concepts/' + conceptId).then((result) => {
                                         encounter.conceptTerm = result.pt.term;
                                     }).catch(function(error){
                                         Ember.set(loading, false);
